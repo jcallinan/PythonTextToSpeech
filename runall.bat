319d44e165d5fc4162fb2a30b818105267a0e2f5
Jeremy Callinan <jeremy.callinan@gmail.com>
2024-03-22 19:00:59 -0400

    Scripts

--- runall.bat
+++ runall.bat
@@ -1,4 +1,2 @@
-python tts2.py Introduction.txt
-python tts2.py Directions.txt
-python tts2.py BreathingExercise.txt
-python tts2.py Outro.txt
+python tts2.py Script1.txt
+python tts2.py Script2.txt
